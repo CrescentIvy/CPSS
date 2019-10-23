9fd67f075c3f78e39da6457be57f7ac24143671c
CrescentIvy <54655474+CrescentIvy@users.noreply.github.com>
2019-10-23 13:48:50 +0800

    Add files via upload

--- model/train_audio.py
+++ model/train_audio.py
@@ -16,11 +16,12 @@ from keras.optimizers import Adam
 #from attention_model import AttentionLayer
 from transformer import Attention, Position_Embedding
 import numpy as np
+from Losshistory import LossHistory
 from tensorflow.python import debug as tf_debug
 #<meta charset="utf-8">
 
 # Parameter setting
-data_path = 'C:\\Users\\NIZI\\Documents\\a.KUKU\\UESTC\\CPSS\\1\\MELD\\MELD.Raw\\data\\'
+data_path = '../data/'
 
 def weight_expand(x):
     return backend.expand_dims(x)
@@ -42,25 +43,24 @@ def get_data(path):
     return train_label, train_audio, test_label, test_audio
 
 num_class = 7
-epoch = 10
-batch_size = 8
+epoch = 20
+batch_size = 32
 head_num = 8
 head_size = 16
 
 # Model Architecture
 # Audio feature vector
-x = Input(shape=(1200, 92))
-print(x.shape)
-#x = Masking(mask_value=0.)(audio_input)
-#x = Position_Embedding()(audio_input)
+audio_input = Input(shape=(1200, 92))
+x = Masking(mask_value=0.)(audio_input)
+x = Position_Embedding()(audio_input)
 
 x = Attention(head_num, head_size)([x, x, x])
 x = BatchNormalization()(x)
 x = Dropout(0.15)(x)
-#
-#x = Attention(head_num, head_size)([x, x, x])
-#x = BatchNormalization()(x)
-#x = Dropout(0.15)(x)
+
+x = Attention(head_num, head_size)([x, x, x])
+x = BatchNormalization()(x)
+x = Dropout(0.15)(x)
 #
 #x = Attention(head_num, head_size)([x, x, x])
 #x = BatchNormalization()(x)
@@ -79,7 +79,7 @@ x = BatchNormalization()(x)
 
 audio_vector = GlobalMaxPooling1D()(x)
 # merge layer
-#print('audio_vector:', audio_vector.shape)
+print('audio_vector:', audio_vector.shape)
 
 # decision-making
 d = Dense(32)(audio_vector)
@@ -89,7 +89,7 @@ d = Dropout(0.5)(d)
 d = Dense(16)(d)
 d = BatchNormalization()(d)
 d = Activation('relu')(d)
-d = Dropout(0.5)(d)
+#d = Dropout(0.5)(d)
 #d = Dense(8)(d)
 #d = BatchNormalization()(d)
 #d = Activation('relu')(d)
@@ -102,11 +102,12 @@ print('prediction shape: ', prediction.shape)
 audio_model = Model(inputs=audio_input, outputs=prediction)
 
 # optimizer
-adam = Adam(lr=0.0000001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+adam = Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 audio_model.compile(loss='categorical_crossentropy', optimizer = adam, metrics=['accuracy'])
 audio_model.summary()
 
 if __name__ == "__main__":
+    history = LossHistory()
     # Audio model training
     # data loader
     train_label, train_audio, test_label, test_audio = get_data(data_path)
@@ -119,13 +120,16 @@ if __name__ == "__main__":
     
     audio_model.fit(train_audio,
                     train_label,
-                    batch_size=batch_size,
-                    epochs=1,
-                    verbose=1)
+                    batch_size = batch_size,
+                    epochs = epoch,
+                    verbose = 1,
+                    validation_data = (test_audio, test_label),
+                    callbacks = [history])
 
     loss_a, acc_a = audio_model.evaluate(test_audio,
                                          test_label,
-                                         batch_size=batch_size,
-                                         verbose=0)
+                                         batch_size = batch_size,
+                                         verbose = 0)
 
-    print('loss_a', loss_a, ' ', 'acc_a', acc_a)
+#    print('loss_a', loss_a, ' ', 'acc_a', acc_a)
+    history.loss_plot('epoch')
